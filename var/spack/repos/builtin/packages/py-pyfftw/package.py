34842273b7e467a1631b06049532bca6086d7ea4
Michaël Michaux <47259765+mmichaux@users.noreply.github.com>
2022-08-02 14:06:27 +0900

    py-pyfftw: added version 0.13.0

--- var/spack/repos/builtin/packages/py-pyfftw/package.py
+++ var/spack/repos/builtin/packages/py-pyfftw/package.py
@@ -13,6 +13,7 @@ class PyPyfftw(PythonPackage):
     homepage = "http://hgomersall.github.com/pyFFTW"
     pypi = "pyFFTW/pyFFTW-0.10.4.tar.gz"
 
+    version("0.13.0", sha256="da85102405c0bd95d57eb19e99b01a0729d8406cb204c3900894b873784253da")
     version("0.12.0", sha256="60988e823ca75808a26fd79d88dbae1de3699e72a293f812aa4534f8a0a58cb0")
     version("0.11.1", sha256="05ea28dede4c3aaaf5c66f56eb0f71849d0d50f5bc0f53ca0ffa69534af14926")
     version("0.10.4", sha256="739b436b7c0aeddf99a48749380260364d2dc027cf1d5f63dafb5f50068ede1a")
